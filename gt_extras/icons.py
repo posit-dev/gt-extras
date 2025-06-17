40a31e7cbd761ee913cbacde9347be4287ee0b1c
Jules <54960783+juleswg23@users.noreply.github.com>
2025-06-17 13:16:26 -0400

    fa_icon_repeat function added

--- gt_extras/icons.py
+++ gt_extras/icons.py
@@ -1,26 +1,43 @@
 from __future__ import annotations
 from typing import Literal
 
-import fontawesome as fa
+import faicons
+
+__all__ = "fa_icon_repeat"
 
 
 def fa_icon_repeat(
     name: str = "star",
     repeats: int = 1,
-    fill: str = "", # todo color
-    fill_opacity: int = 1,
-    stroke: str = "", # todo color
-    stroke_width: int = 1,
-    stroke_opacity: int = 1,
+    fill: str | None = None,
+    fill_opacity: str | None = None,
+    stroke: str | None = None,
+    stroke_width: str | None = None,
+    stroke_opacity: str | None = None,
     height: str | None = None,
     width: str | None = None,
-    margin_left: int | None = None,
-    margin_right: int | None = None,
-    position: Literal["relative"] | None = None, # todo opts
-    title:  str | None = None,
+    margin_left: str | None = None,
+    margin_right: str | None = None,
+    position: str | None = None,
+    title: str | None = None,
     a11y: Literal["deco", "sem"] | None = None,
 ) -> str:
-    res = fa.icons['thumbs-up']
-    print(fa.icons[name])
+    icon = faicons.icon_svg(
+        name=name,
+        fill=fill,
+        fill_opacity=fill_opacity,
+        stroke=stroke,
+        stroke_width=stroke_width,
+        stroke_opacity=stroke_opacity,
+        height=height,
+        width=width,
+        margin_left=margin_left,
+        margin_right=margin_right,
+        position=position,
+        title=title,
+        a11y=a11y,
+    )
 
-    return res
+    repeated_icon = str(icon) * repeats
+
+    return repeated_icon
