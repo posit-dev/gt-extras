28100c9b4aad5e759077bf0f607a4e70a3ac672d
Jules <54960783+juleswg23@users.noreply.github.com>
2025-07-01 16:44:25 -0400

    setup

---
+++ gt_extras/styling.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from great_tables import GT
+
+__all__ = ["gt_add_divider"]
+
+
+def gt_add_divider(gt: GT) -> GT:
+    pass
---
+++ gt_extras/tests/test_styling.py
@@ -0,0 +1,2 @@
+def test_gt_add_divider_basic():
+    pass
